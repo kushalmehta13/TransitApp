732396ef2f8d00f98c7c6e876a4b0c4dfdf36840
kushal <kushalmehta13@gmail.com>
2019-03-25 15:42:21 -0400

    Transit App created with UI built till Driver Dashboard

---
+++ app/src/main/java/com/example/transitapp/DriverDashboard.java
@@ -0,0 +1,105 @@
+package com.example.transitapp;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DialogTitle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.List;
+
+public class DriverDashboard extends AppCompatActivity implements View.OnClickListener {
+    private Button signOut;
+    private Intent login;
+    private Button preInspection;
+    private Button beginInspection;
+    private RelativeLayout layout;
+    private RelativeLayout mainLayout;
+    private PopupWindow popUp;
+    private AutoCompleteTextView bus_num;
+    private TextView driverName;
+    private View parent, popupView;
+    private PopupWindow popupWindow;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_dashboard);
+
+        login = new Intent(this, FirebaseUIActivity.class);
+        signOut = findViewById(R.id.signOut);
+        preInspection = findViewById(R.id.pre_insp_check);
+        signOut.setOnClickListener(this);
+        preInspection.setOnClickListener(this);
+        driverName = findViewById(R.id.driver_name);
+        parent = findViewById(R.id.driverDashboard);
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        driverName.setText("Hello, " + user.getDisplayName());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.signOut: AuthUI.getInstance().signOut(getApplicationContext()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    startActivity(login);
+                    finish();
+                }
+            });
+                    break;
+            case R.id.pre_insp_check:
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                popupView = inflater.inflate(R.layout.popup, null);
+                boolean focusable = true;
+                popupWindow = new PopupWindow(popupView, 800, 800, focusable);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    popupWindow.setElevation(20);
+                }
+                popupWindow.showAtLocation(parent, Gravity.CENTER, 0,0 );
+
+                bus_num = popupView.findViewById(R.id.bus_numbers);
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                        R.array.planets_array, R.layout.spinner_item);
+                adapter.setDropDownViewResource(R.layout.spinner_item);
+                bus_num.setAdapter(adapter);
+                bus_num.setHint("Bus number");
+
+                beginInspection = popupView.findViewById(R.id.beginInspection);
+                beginInspection.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // TODO: Create new intent for pre inspection and start activity
+
+                        // Place holder for now
+                        popupWindow.dismiss();
+                    }
+                });
+                    break;
+        }
+
+    }
+}
